ef0e0834c03cb8c6aae94d752f313d2bc406fbfb
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-15 17:09:30 +0530

    Create Person, student and teacher class implemented OOPS concepts

---
+++ person.rb
@@ -0,0 +1,22 @@
+class Person
+  def iinitialize(name, age = 'unknown', parent_permission = 'true')
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # getter
+  attr_accessor :name
+  attr_accessor :age
+  attr_reader :parent_permission
+
+  def of_age?
+    @age >= 18
+  end
+
+  private :of_age?
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
