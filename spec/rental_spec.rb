ec7e1a8bf897374ed8c09fa83dd1399c61a7d42c
unknown <sharonkessio6@gmail.com>
2022-11-25 10:57:47 +0300

    test for classroom and rental

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,12 +1,15 @@
-require_relative '../classroom'
-describe Classroom do
-    before :each do
-        @classroom = Classroom.new('Classroom 1')
+require_relative '../rental'
+require_relative '../person'
+require_relative '../book'
+describe Rental do
+  let (:rental) { Rental.new('2022-11-25', Person.new('John Doe', 18), Book.new('The Hobbit', 'J.R.R. Tolkien')) }
+    it 'Rental should be instance of Rental' do
+        expect(rental).to be_instance_of(Rental)
     end
-    it 'Classroom label should be Classroom 1' do
-        expect(@classroom.label).to eq('Classroom 1')
+    it 'Rental date should be 2022-11-25' do
+        expect(rental.date).to eq('2022-11-25')
     end
-    it 'Classroom should be instance of Classroom' do
-        expect(@classroom).to be_instance_of(Classroom)
+    it "Rental should have a book" do
+        expect(rental.book).to be_instance_of(Book)
     end
-end
+    end
