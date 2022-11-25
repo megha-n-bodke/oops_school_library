2b36f65d6ebfa741034ab78418ba745e362fbd41
unknown <sharonkessio6@gmail.com>
2022-11-25 11:00:22 +0300

    fix linter erros

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -2,14 +2,14 @@ require_relative '../rental'
 require_relative '../person'
 require_relative '../book'
 describe Rental do
-  let (:rental) { Rental.new('2022-11-25', Person.new('John Doe', 18), Book.new('The Hobbit', 'J.R.R. Tolkien')) }
-    it 'Rental should be instance of Rental' do
-        expect(rental).to be_instance_of(Rental)
-    end
-    it 'Rental date should be 2022-11-25' do
-        expect(rental.date).to eq('2022-11-25')
-    end
-    it "Rental should have a book" do
-        expect(rental.book).to be_instance_of(Book)
-    end
-    end
+  let(:rental) { Rental.new('2022-11-25', Person.new('John Doe', 18), Book.new('The Hobbit', 'J.R.R. Tolkien')) }
+  it 'Rental should be instance of Rental' do
+    expect(rental).to be_instance_of(Rental)
+  end
+  it 'Rental date should be 2022-11-25' do
+    expect(rental.date).to eq('2022-11-25')
+  end
+  it 'Rental should have a book' do
+    expect(rental.book).to be_instance_of(Book)
+  end
+end
